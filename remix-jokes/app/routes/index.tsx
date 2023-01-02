7b1874b14a3d7856a0a84ae102f5b31b5307295b
prodgers1 <pcrodgers1@gmail.com>
2023-01-02 06:04:38 +0000

    Use correct homepage

--- remix-jokes/app/routes/index.tsx
+++ remix-jokes/app/routes/index.tsx
@@ -1,29 +1,27 @@
-import type { LoaderArgs } from "@remix-run/node";
-import { json } from "@remix-run/node";
-import { useLoaderData, Link } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
 
-import { db } from "~/utils/db.server";
+import stylesUrl from "~/styles/index.css";
 
-export const loader = async () => {
-  const count = await db.joke.count();
-  const randomRowNumber = Math.floor(Math.random() * count);
-  const [randomJoke] = await db.joke.findMany({
-    take: 1,
-    skip: randomRowNumber,
-  });
-  return json({ randomJoke });
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: stylesUrl }];
 };
 
-export default function JokesIndexRoute() {
-  const data = useLoaderData<typeof loader>();
-
+export default function IndexRoute() {
   return (
-    <div>
-      <p>Here's a random joke:</p>
-      <p>{data.randomJoke.content}</p>
-      <Link to={data.randomJoke.id}>
-        "{data.randomJoke.name}" Permalink
-      </Link>
+    <div className="container">
+      <div className="content">
+        <h1>
+          Remix <span>Jokes!</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="jokes">Read Jokes</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
